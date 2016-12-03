7f92ac8b3bb75bad79f948117e9b28ada4fc028d
Michalina Oleksy <layika@protonmail.com>
2016-12-03 12:56:06 +0100

    Update triangles.py

--- day03/triangles.py
+++ day03/triangles.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env/ python
 
+def is_triangle(a, b, c):
+    if a+b>c and b+c>a and a+c>b:
+        return True
+    return False
+
 def count_triangles(triangles):
     triangles_count = 0
     for triangle in triangles.split('\n'):
@@ -7,7 +12,7 @@ def count_triangles(triangles):
         a = int(sides[0])
         b = int(sides[1])
         c = int(sides[2])
-        if a+b>c and b+c>a and a+c>b:
+        if is_triangle(a, b, c):
             triangles_count += 1
     return triangles_count
 
