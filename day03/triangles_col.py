bb8ffb20dc0a1f6bee570efa36a4bda727319841
Michalina Oleksy <layika@protonmail.com>
2016-12-03 09:28:49 +0100

    Added solution for the second part

---
+++ day03/triangles_col.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env/ python
+
+import re
+
+def chunks(row, n):
+    return (row[i:i+n] for i in xrange(0, len(row), n))
+
+def is_a_triangle(triangles):
+    triangles_count = 0
+    for triangle in chunks(triangles, 3):
+        a = int(triangle[0])
+        b = int(triangle[1])
+        c = int(triangle[2])
+        if a+b>c and b+c>a and a+c>b:
+            triangles_count += 1
+    return triangles_count
+
+def solve(triangles):
+    triangles_count = 0
+    col_0 = []
+    col_1 = []
+    col_2 = []
+    
+    for triangle in triangles.split('\n'):
+        vals =  triangle.split()
+        col_0.append(vals[0])
+        col_1.append(vals[1])
+        col_2.append(vals[2])
+        
+    for column in (col_0, col_1, col_2):
+        triangles_count += is_a_triangle(column)
+    return triangles_count
+
+
+with open('test_input') as f:
+    triangles = f.read()
+assert solve(triangles) == 9
+
+with open('input') as f:
+    triangles = f.read()
+print "COL: There are", solve(triangles), "triangles in the input file."
